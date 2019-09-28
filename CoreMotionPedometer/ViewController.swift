90c4bdd0dafcb80384097a839138324d5903df05
Forlefac Fontem <fontem.2@osu.edu>
2019-09-28 00:55:39 -0400

    Initial Commit

---
+++ CoreMotionPedometer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMotionPedometer
+//
+//  Created by Forlefac Fontem on 9/28/19.
+//  Copyright © 2019 Forlefac Fontem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
